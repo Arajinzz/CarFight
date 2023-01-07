71e268e3a2784cb563098c12b331cd317a875147
arajinzz <aladdinn360@gmail.com>
2023-01-08 00:53:03 +0100

    adding simple acceleration

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,17 +4,22 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+    
+    private Rigidbody carRb;
 
     public GameObject[] SuspensionPoints;
     public float SuspensionDistance = 0.5f;
-    public float SuspensionStrength = 100000.0f;
-    public float SuspensionDamping = 100;
+    public float SuspensionStrength = 100.0f;
+    public float SuspensionDamping = 2;
 
     private Ray[] SuspensionDebugRays;
     private RaycastHit[] SuspensionDebugHit;
 
-    private Rigidbody carRb;
+    public float AccelPower = 200.0f; 
 
+    // Input state
+    bool isAccel = false;
+    bool isBrake = false;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +30,21 @@ public class CarController : MonoBehaviour
 
         carRb = GetComponent<Rigidbody>();
 
+        // Extra protection
+        isAccel = false;
+        isBrake = false;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        isAccel = Input.GetKey(KeyCode.W);
+        isBrake = Input.GetKey(KeyCode.S);
+
+        // If we're accelerating override Brake (don't brake)
+        // This is just to test smoothly, it will be changed
+        isBrake = isAccel ? false : isBrake;
     }
 
     // We handle physics here
@@ -60,6 +74,22 @@ public class CarController : MonoBehaviour
                 carRb.AddForceAtPosition(direction * force, suspensionPt.transform.position);
             }
 
+        }
+
+        // THERE IS A FLAW HERE
+        // THE CAR AXIS ARE MESSED UP IN WORLD SPACE
+        // IT NEEDS TO BE FIXED
+        if (isAccel)
+        {
+            // Will just apply some force here
+            // But i think it needs more work, because it takes time for a vehicle to reach max speed
+            // So i think it needs a rework (a way to properly simulate acceleration)
+            carRb.AddForceAtPosition(transform.right * AccelPower, transform.position); // Center of mass is center of vehicle CURRENTLY
+        }
+
+        if (isBrake)
+        {
+            carRb.AddForceAtPosition(-transform.right * AccelPower, transform.position); // Center of mass is center of vehicle CURRENTLY
         }
 
     }
