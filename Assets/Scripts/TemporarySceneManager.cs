b5ad0918a6b11bd2b1caef4f13751ab99ffce0a7
arajinzz <aladdinn360@gmail.com>
2023-01-25 23:23:01 +0100

    try to join a lobby if there's any otherwise create one, or simply load the game scene if an unexpected error occurs

--- Assets/Scripts/TemporarySceneManager.cs
+++ Assets/Scripts/TemporarySceneManager.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Steamworks.Data;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TemporarySceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    async void Start()
     {
-        
+        try
+        {
+            await SteamLobbyManager.Instance.SearchLobbies("JustTesting");
+
+            List<Lobby> lobbiesFound = SteamLobbyManager.Instance.LobbiesResult;
+
+            if (lobbiesFound != null && lobbiesFound.Count > 0)
+            {
+                await SteamLobbyManager.Instance.JoinLobby(lobbiesFound[0]);
+            } else
+            {
+                await SteamLobbyManager.Instance.CreateLobby(4, "JustTesting");
+            }
+
+        } catch
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
