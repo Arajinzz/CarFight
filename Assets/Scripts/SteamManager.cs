d622e5c306a482afa7278839bd30e1750e95caa2
arajinzz <aladdinn360@gmail.com>
2023-01-25 20:46:36 +0100

    SteamManager will be a singeleton

--- Assets/Scripts/SteamManager.cs
+++ Assets/Scripts/SteamManager.cs
@@ -5,7 +5,23 @@ using UnityEngine;
 
 public class SteamManager : MonoBehaviour
 {
-    
 
+    public static SteamManager Instance;
+
+    private static uint AppId = 480u;
+
+    private void Awake()
+    {
+        
+        if (Instance == null)
+        {
+            DontDestroyOnLoad(this);
+            Instance = this;
+
+
+
+        }
+
+    }
 
 }
