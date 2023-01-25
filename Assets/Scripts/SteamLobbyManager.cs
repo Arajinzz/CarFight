b9997bdab557caf96a07ec54328dd3413c71666f
arajinzz <aladdinn360@gmail.com>
2023-01-25 22:31:11 +0100

    adding callback bodies

--- Assets/Scripts/SteamLobbyManager.cs
+++ Assets/Scripts/SteamLobbyManager.cs
@@ -41,8 +41,6 @@ public class SteamLobbyManager : MonoBehaviour
     }
 
 
-
-
     #region Help functions
 
     private void CleanUp()
@@ -62,8 +60,79 @@ public class SteamLobbyManager : MonoBehaviour
     #endregion
 
 
+    #region Callbacks
+
+    // Executed when a lobby is created
+    private void OnLobbyCreatedCallback(Result result, Lobby lobby)
+    {
+        
 
 
+    }
+
+    // Executed only when a member Disconnected
+    // Which means when i get Disconnected it will not get executed 
+    private void OnLobbyMemberDisconnectedCallback(Lobby lobby, Friend member)
+    {
+
+
+
+    }
+
+    // Executed only when a member leaves
+    // Which means when i leave it will not get executed 
+    private void OnLobbyMemberLeaveCallback(Lobby lobby, Friend member)
+    {
+        
+
+
+    }
+
+    // Executed when a member joins the lobby
+    private void OnLobbyMemberJoinedCallback(Lobby lobby, Friend member)
+    {
+        
+    }
+
+    // Executed when we enter the lobby
+    private void OnLobbyEnteredCallback(Lobby lobby)
+    {
+        
+
+
+    }
+
+    private void OnLobbyInviteCallback(Friend member, Lobby lobby)
+    {
+
+    }
+
+    // When Game is started
+    private void OnLobbyGameCreatedCallback(Lobby lobby, uint ip, ushort port, SteamId gameServerId)
+    {
+        // Load game scene
+
+    }
+
+    // Executed when we receive a chat message
+    private void OnChatMessageCallback(Lobby lobby, Friend friend, string message)
+    {
+        // Received chat message
+        
+    }
+
+    private void OnP2PSessionRequestCallback(SteamId user)
+    {
+
+    }
+
+    // Executed when a scene is loaded
+    private void OnSceneLoadedCallback(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        
+    }
+
+    #endregion
 
 
 }
