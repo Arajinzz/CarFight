01c1458d293a93ccd421e7110f27f1099ad3884f
arajinzz <aladdinn360@gmail.com>
2023-01-25 23:49:11 +0100

    adding some ui feedback to know if we're in a lobby and if we're hosting the lobby

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField]
+    TMP_Text LobbyIdTxt;
+
+    [SerializeField]
+    TMP_Text HostingTxt;
+
+    [SerializeField]
+    TMP_Text TickTxt;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,18 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SteamLobbyManager.Instance)
+        {
+            LobbyIdTxt.SetText(SteamLobbyManager.Instance.CurrentLobby.Id.ToString());
+
+            if (SteamLobbyManager.Instance.CurrentLobby.Owner.Id.Value == SteamManager.Instance.PlayerId.Value)
+            {
+                HostingTxt.SetText("Host");
+            } else
+            {
+                HostingTxt.SetText("Client");
+            }
+
+        }
     }
 }
