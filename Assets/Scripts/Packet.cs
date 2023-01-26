0d968642f8b0db5539d12debdbf518d1c825413c
arajinzz <aladdinn360@gmail.com>
2023-01-26 17:14:25 +0100

    packet constructor

--- Assets/Scripts/Packet.cs
+++ Assets/Scripts/Packet.cs
@@ -5,7 +5,32 @@ using UnityEngine;
 
 public class Packet
 {
-    
-    
+
+    public enum PacketType : ushort
+    {
+        InstantiatePlayer,
+        InputMessage,
+        StateMessage,
+    }
+
+    public UInt16 packetType;
+    public List<byte> buffer;
+    public int offset;
+
+    public Packet(PacketType type)
+    {
+        packetType = Convert.ToUInt16(type);
+        buffer = new List<byte>();
+        buffer.AddRange(BitConverter.GetBytes(packetType));
+    }
+
+    public Packet(byte[] data)
+    {
+        buffer = new List<byte>(data);
+        packetType = BitConverter.ToUInt16(buffer.GetRange(offset, sizeof(ushort)).ToArray());
+        offset += sizeof(ushort);
+    }
+
+
 
 }
