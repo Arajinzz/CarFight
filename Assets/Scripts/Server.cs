aa0dd2bd8aa724c78a3082d2922c3372323744f0
arajinzz <aladdinn360@gmail.com>
2023-01-26 18:35:56 +0100

    declaring some important variable

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Steamworks;
+using Steamworks.Data;
 using UnityEngine;
 
 public class Server : MonoBehaviour
 {
+    // to run at fixed tick rate
+    private float serverTimer;
+    public uint serverTick;
+    private float minTimeBetweenTicks;
+    private const float SERVER_TICK_RATE = 60f;
+    
+    private Queue<P2Packet?> receivedPackets;
+
+    private Lobby currentLobby;
+    private Friend owner;
+
     // Start is called before the first frame update
     void Start()
     {
