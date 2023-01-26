9f30b04a18b4468bbbf99bb7d923ab806595bc64
arajinzz <aladdinn360@gmail.com>
2023-01-26 17:10:11 +0100

    add input structs

--- Assets/Scripts/Structs.cs
+++ Assets/Scripts/Structs.cs
@@ -4,7 +4,21 @@ using UnityEngine;
 
 public class Structs
 {
-    
 
+    public struct Inputs
+    {
+        public bool up;
+        public bool down;
+        public bool left;
+        public bool right;
+
+    }
+
+    public struct InputMessage
+    {
+        public float delivery_time;
+        public uint tick_number;
+        public Inputs inputs;
+    }
 
 }
